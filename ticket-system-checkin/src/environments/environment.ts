7c74507ffdf545603506f3c6d21de6db22c146f6
Basil K Baby <basil@mailcohen.com>
2026-02-20 21:35:14 +0000

    commit changes

--- ticket-system-checkin/src/environments/environment.ts
+++ ticket-system-checkin/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7144', //https://v4entertainment.azurewebsites.net
+  apiUrl: 'https://v4entertainment.azurewebsites.net', //https://v4entertainment.azurewebsites.net
   appUrl: 'https://checkin.v4entertainments.co.uk',
   enableAnalytics: false
 };
